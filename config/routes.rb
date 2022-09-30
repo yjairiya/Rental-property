2e1670f208d799f96f66e4df305b4ad3144cd9f4
developer <you@example.com>
2022-09-30 16:24:09 +0530

    Added login signup and navbar views

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
   root 'posts#index'
   get 'admins/index'
-
 
   namespace :admin do
     resources :roles
     resources :users
     root to: "roles#index"
   end
-
   devise_for :users
-   
   resources :posts
-  resources :users  
+  resources :users
 
-
+  get '/about', to: "posts#about" 
+  get '/contact', to: "posts#contact"
   # devise_for :users, controllers: {
   #   sessions: 'users/sessions'
   # }
