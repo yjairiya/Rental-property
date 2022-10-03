e8083c086024079b6000951bb2ac22c2951dc600
developer <you@example.com>
2022-10-03 13:02:57 +0530

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   # has_one_attached :image, dependent: :destroy
+  validates :location, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
+  validates :phone, presence: true
+  validates :owner_name, presence: true
   has_one_attached :image
-
   belongs_to :user
 end
