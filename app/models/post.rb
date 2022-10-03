ad2a7a8795b7050b0f6c078d760f6c24d9c95beb
yjairiya <yjairiya@bestpeers.com>
2022-10-03 13:25:10 +0530

    Add multiple images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   # has_one_attached :image, dependent: :destroy
-  has_one_attached :image
+  has_many_attached :images, dependent: :destroy
 
   belongs_to :user
 end
