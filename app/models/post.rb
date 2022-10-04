f6bccec401faec51c8124283d7cf91d4f2ee2d71
yjairiya <yjairiya@bestpeers.com>
2022-10-04 12:14:58 +0530

    conflic error solve

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,19 +3,12 @@ class Post < ApplicationRecord
   has_many_attached :images
   belongs_to :user
 
+  enum status: [:pending, :approved, :rejected ]
+  
   validates :location, presence: true
   validates :size, presence: true
   validates :price, presence: true
   validates :phone, presence: true
   validates :owner_name, presence: true
-  
-  enum status: [:pending, :approved, :rejected]
-
-#   def initialize(user)
-#     user ||= User.new # guest user (not logged in)
-#     if user.admin?
-#       can :update, User
-#     end
-#  end
 
 end
--- db/migrate/20221003113043_add_status_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :status, :integer, default: 0
-  end
-end
